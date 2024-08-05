3fe542c5a5c12ba0394cd2ae54a59ecde32c346c
Roland Haas <rhaas@illinois.edu>
2024-08-05 16:27:28 -0500

    openPMD: re-add workaround when CMake does not detect HDF5 version
    the solution included in openPMD is not sufficient on Perlmutter

--- src/build.sh
+++ src/build.sh
@@ -41,6 +41,7 @@ ${TAR?} xf ${SRCDIR}/../dist/${NAME}.tar
 
 echo "openPMD: Applying patches..."
 pushd ${NAME}
+${PATCH?} -p1 < ${SRCDIR}/../dist/hdf5_version.patch
 ${PATCH?} -p1 < ${SRCDIR}/../dist/explicit_specialization.patch
 # Some (ancient but still used) versions of patch don't support the
 # patch format used here but also don't report an error using the exit
